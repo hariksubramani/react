45b34c87f33056bf7b56e1eedd8c5a6f570a854d
TCIG-Hari <hari@mailcohen.com>
2019-05-19 01:08:37 +0300

    adding the package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Create from './components/create.component';
+import Edit from './components/edit.component';
+import Index from './components/index.component';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (   
+    <Router>
+    <div className="container">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <Link to={'/'} className="navbar-brand">React Crud Example</Link>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+    
+    <li className="nav-item">
+    <Link to={'/'} className="nav-link">
+    Home
+    </Link>
+    <Link to={'/create'} className="nav-link">
+    create
+    </Link>
+    <Link to={'/index'} className="nav-link">
+    index
+    </Link>
+    </li>
+    </ul>
     </div>
+    </nav>
+    <br/>
+
+    <h2>
+    Welcome to React CRUD Tutorial
+    </h2>
+
+
+    <Switch>
+
+    <Route exact path="/create" component={Create}></Route>
+    <Route path="/index" component={Index}></Route>
+    <Route path="/edit/:id" component={Edit}></Route>
+
+    </Switch>
+
+    </div>
+    </Router>
   );
 }
 
